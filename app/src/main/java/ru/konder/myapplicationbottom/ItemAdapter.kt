4f287c3e2876efe225eb7b64e10c1ee73de228d2
aelxb <67102261+aelxb@users.noreply.github.com>
2020-11-05 09:20:30 +0300

    Add files via upload

---
+++ app/src/main/java/ru/konder/myapplicationbottom/ItemAdapter.kt
@@ -0,0 +1,40 @@
+package ru.konder.myapplicationbottom
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ItemAdapter(
+
+    private val context: Context?,
+    private val images: List<ItemOfList>,
+    val listener: (ItemOfList) -> Unit
+
+): RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val imageSrc = view.findViewById<ImageView>(R.id._image)
+        val tvTitle = view.findViewById<TextView>(R.id.title)
+        fun bind(image: ItemOfList, listener: (ItemOfList) -> Unit){
+            imageSrc.setImageResource(image.imageSrc)
+            tvTitle.text = image.imageTitle
+            itemView.setOnClickListener{
+                (listener(image))
+            }
+        }
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(images[position], listener)
+    }
+
+    override fun getItemCount(): Int {
+        return images.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+        ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_of_list, parent, false))
+}
